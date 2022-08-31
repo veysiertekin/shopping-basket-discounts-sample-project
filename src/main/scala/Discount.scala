5837ae82211ad7d6eed94adc1175dfc15fffc584
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 13:16:55 +0100

    Refactor discount logic

--- src/main/scala/Discount.scala
+++ src/main/scala/Discount.scala
@@ -1,27 +1,28 @@
-sealed trait Discount {
+sealed abstract class Discount(itemName: String) {
   def calculate(basket: Basket, basePrices: Map[String, Double] = Map()): Double
-}
-case class ThreeForTwoDiscount(itemName: String) extends Discount {
-  override def calculate(basket: Basket, basePrices: Map[String, Double]): Double = {
-    val filteredItems = basket.items.filter(_ == itemName)
-    if (filteredItems.isEmpty) {
-      0
-    } else {
-      val numberOfElements  = filteredItems.size
-      val discountOccurance = numberOfElements / 3
-      basePrices(itemName) * discountOccurance
-    }
-  }
-}
-case class BuyOneGetOneDiscount(itemName: String) extends Discount {
-  override def calculate(basket: Basket, basePrices: Map[String, Double] = Map()): Double = {
+
+  protected def calculateByOccurance(
+    basket: Basket,
+    basePrices: Map[String, Double],
+    occurance: Int
+  ): Double = {
     val filteredItems = basket.items.filter(_ == itemName)
     if (filteredItems.isEmpty) {
       0d
     } else {
       val numberOfElements  = filteredItems.size
-      val discountOccurance = numberOfElements / 2
+      val discountOccurance = numberOfElements / occurance
       basePrices(itemName) * discountOccurance
     }
+  }
+}
+case class ThreeForTwoDiscount(itemName: String) extends Discount(itemName) {
+  override def calculate(basket: Basket, basePrices: Map[String, Double]): Double = {
+    calculateByOccurance(basket, basePrices, 3)
+  }
+}
+case class BuyOneGetOneDiscount(itemName: String) extends Discount(itemName) {
+  override def calculate(basket: Basket, basePrices: Map[String, Double] = Map()): Double = {
+    calculateByOccurance(basket, basePrices, 2)
   }
 }
