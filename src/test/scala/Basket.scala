5e37f777199b0a08f5102dcf15c8744ea127ed08
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 11:40:48 +0100

    Refactor - use init+last recursion instead of head+tail

--- src/test/scala/Basket.scala
+++ src/test/scala/Basket.scala
@@ -4,15 +4,16 @@ case class Basket(items: List[String] = List()) {
     this.copy(items = items :+ item)
   }
   def remove(item: String): Basket = {
-    this.copy(items = removefirst(item, items.reverse).reverse)
+    this.copy(items = removeLast(item, items))
   }
 
   @tailrec
-  private def removefirst(item: String, items: List[String], processedItems: List[String] = List()): List[String] = {
-    items match {
-      case Nil                            => processedItems
-      case head :: tail if (head == item) => processedItems ++ tail
-      case head :: tail                   => removefirst(item, tail, processedItems :+ head)
-    }
+  private def removeLast(item: String, items: List[String], processedItems: List[String] = List()): List[String] = {
+    if (items == Nil)
+      processedItems
+    else if (items.last == item)
+      processedItems ++ items.init
+    else
+      removeLast(item, items.init, processedItems :+ items.last)
   }
 }
