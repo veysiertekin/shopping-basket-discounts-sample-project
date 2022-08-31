143911750ab999e0c1d71ba62f56a1a8d7bc6944
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 17:47:52 +0100

    Clean up recursion in Basket

--- src/main/scala/Basket.scala
+++ src/main/scala/Basket.scala
@@ -1,19 +1,23 @@
 import scala.annotation.tailrec
+
 case class Basket(items: List[String] = List()) {
   def add(item: String): Basket = {
     this.copy(items = items :+ item)
   }
   def remove(item: String): Basket = {
-    this.copy(items = removeLast(item, items))
+    this.copy(items = removeLastMatch(item, items))
   }
 
   @tailrec
-  private def removeLast(item: String, items: List[String], processedItems: List[String] = List()): List[String] = {
-    if (items == Nil)
-      processedItems
-    else if (items.last == item)
-      processedItems ++ items.init
-    else
-      removeLast(item, items.init, processedItems :+ items.last)
+  private def removeLastMatch(
+    item: String,
+    items: List[String],
+    processedItems: List[String] = List()
+  ): List[String] = {
+    items match {
+      case Nil                         => processedItems
+      case list if (list.last == item) => list.init ++ processedItems
+      case list                        => removeLastMatch(item, list.init, list.last +: processedItems)
+    }
   }
 }
