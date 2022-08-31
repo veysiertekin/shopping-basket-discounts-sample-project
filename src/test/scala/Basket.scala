7d8fe3a8254b70bee252c3bc08f2129133ad91a6
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 11:24:12 +0100

    Refactor - optimize removal process with tail recursion

--- src/test/scala/Basket.scala
+++ src/test/scala/Basket.scala
@@ -1,16 +1,18 @@
+import scala.annotation.tailrec
 case class Basket(items: List[String] = List()) {
   def add(item: String): Basket = {
     this.copy(items = items :+ item)
   }
   def remove(item: String): Basket = {
-    def removefirst(item: String, items: List[String]): List[String] = {
-      items match {
-        case Nil                            => List()
-        case head :: tail if (head == item) => tail
-        case head :: tail                   => head :: removefirst(item, tail)
-      }
-    }
-
     this.copy(items = removefirst(item, items))
+  }
+
+  @tailrec
+  private def removefirst(item: String, items: List[String], processedItems: List[String] = List()): List[String] = {
+    items match {
+      case Nil                            => processedItems
+      case head :: tail if (head == item) => processedItems ++ tail
+      case head :: tail                   => removefirst(item, tail, processedItems :+ head)
+    }
   }
 }
