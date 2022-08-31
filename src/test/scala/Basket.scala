d3a90761d77315c51394908714404595039748ca
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 10:57:57 +0100

    'remove non-existent item from empty basket' implementation

--- src/test/scala/Basket.scala
+++ src/test/scala/Basket.scala
@@ -2,4 +2,7 @@ case class Basket(items: List[String] = List()) {
   def add(item: String): Basket = {
     this.copy(items = items :+ item)
   }
+  def remove(item: String): Basket = {
+    this
+  }
 }
