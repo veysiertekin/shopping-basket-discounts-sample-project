e4905bbd0539a1533ee311d6efc7cf94b4097e52
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 12:50:25 +0100

    'buy one get one apple free for empty basket' implementation

--- src/main/scala/Discount.scala
+++ src/main/scala/Discount.scala
@@ -3,8 +3,12 @@ sealed trait Discount {
 }
 case object BuyOneGetOneDiscount extends Discount {
   override def calculate(basket: Basket, basePrices: Map[String, Double] = Map()): Double = {
-    val numberOfElements  = basket.items.size
-    val discountOccurance = numberOfElements / 2
-    basePrices(basket.items.head) * discountOccurance
+    if (basket.items.isEmpty) {
+      0d
+    } else {
+      val numberOfElements  = basket.items.size
+      val discountOccurance = numberOfElements / 2
+      basePrices(basket.items.head) * discountOccurance
+    }
   }
 }
