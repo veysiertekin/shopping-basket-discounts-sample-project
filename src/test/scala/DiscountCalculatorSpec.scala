94aea7be5e5c290e0070fea6f7886414e40a82a0
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 12:49:50 +0100

    'buy one get one apple free for empty basket' test case

--- src/test/scala/DiscountCalculatorSpec.scala
+++ src/test/scala/DiscountCalculatorSpec.scala
@@ -3,6 +3,11 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class DiscountCalculatorSpec extends AnyFreeSpec with Matchers {
   "Shop's discount implementations" - {
+    "buy one get one apple free for empty basket" in {
+      val basket   = Basket()
+      val discount = BuyOneGetOneDiscount.calculate(basket, Map("apple" -> 0.6d))
+      discount mustBe 0
+    }
     "buy one get one apple free for just one apple" in {
       val basket   = Basket().add("apple")
       val discount = BuyOneGetOneDiscount.calculate(basket, Map("apple" -> 0.6d))
