a07b7d08e068a761a0eb06bd754718e928bb16a5
veysiertekin <500893+veysiertekin@users.noreply.github.com>
2022-08-31 12:51:29 +0100

    'buy one get one apple free for two apples and an orange in a basket' test  case

--- src/test/scala/DiscountCalculatorSpec.scala
+++ src/test/scala/DiscountCalculatorSpec.scala
@@ -18,5 +18,10 @@ class DiscountCalculatorSpec extends AnyFreeSpec with Matchers {
       val discount = BuyOneGetOneDiscount.calculate(basket, Map("apple" -> 0.6d))
       discount mustBe 0.6d
     }
+    "buy one get one apple free for two apples and an orange in a basket" in {
+      val basket   = Basket().add("apple").add("apple").add("orange")
+      val discount = BuyOneGetOneDiscount.calculate(basket, Map("apple" -> 0.6d))
+      discount mustBe 0.6d
+    }
   }
 }
